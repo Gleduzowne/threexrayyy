edc80ce0f1fc61c5c4f5f6c6b2929ec73d7209e5
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-02-04 23:07:45 +0100

    ✨ Introduce new library `chemistry_plugin` with experimental chemistry capabilities.

---
+++ lib/extensions/chemistry_plugin.dart
@@ -0,0 +1,10 @@
+import 'plugin_interface.dart';
+
+class ChemistryPlugin implements Plugin {
+  @override
+  void register() {
+    // Registration logic for ChemistryPlugin
+  }
+}
+
+// ...existing/placeholder code...
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'views/home_view.dart';
+import 'extensions/plugin_interface.dart';
+import 'extensions/chemistry_plugin.dart';
+import 'extensions/computer_plugin.dart';
+import 'extensions/mathematics_plugin.dart';
+import 'extensions/physics_plugin.dart';
 
 void main() {
   runApp(MyApp());
+  PluginManager().initializePlugins();
 }
 
 class MyApp extends StatelessWidget {
@@ -15,5 +21,23 @@ class MyApp extends StatelessWidget {
       ),
       home: HomeView(),
     );
+  }
+}
+
+class PluginManager {
+  final List<Plugin> _plugins = [];
+
+  void registerPlugin(Plugin plugin) {
+    plugin.register();
+    _plugins.add(plugin);
+  }
+
+  void initializePlugins() {
+    // Register plugins. In a real-world scenario these
+    // could be dynamically discovered.
+    registerPlugin(ChemistryPlugin());
+    registerPlugin(ComputerPlugin());
+    registerPlugin(MathematicsPlugin());
+    registerPlugin(PhysicsPlugin());
   }
 }
