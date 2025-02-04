a56cc375e733585a7fe38efb088cda80f1b84b17
nathfavour <116535483+nathfavour@users.noreply.github.com>
2025-02-04 22:01:19 +0100

    🎨 feat: Added a simple test case to the main function of lib/main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,19 @@
+import 'package:flutter/material.dart';
+import 'views/home_view.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '3D and 2D Animation Engine',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomeView(),
+    );
+  }
+}
